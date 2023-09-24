ce507d8bb47cc19805e9fb1c805038e0c19521e2
RAJAT KUMAR SWAIN <145832002+RAJATKSWAIN@users.noreply.github.com>
2023-09-24 15:06:23 +0530

    Update PKG_LMS_ERRORHANDELER_SPECIFICATION.sql
    2nd commit for change in specification.

--- LMS_PACKAGE_1.0/PKG_LMS_ERRORHANDELER_SPECIFICATION.sql
+++ LMS_PACKAGE_1.0/PKG_LMS_ERRORHANDELER_SPECIFICATION.sql
@@ -1,20 +1,20 @@
 create or replace PACKAGE PKG_LMS_ERRORHANDELER AS
 -- **************************************************************************************************************************
-  --                  |                                                                                        			    *
-  -- PACKAGE HEADER   | PKG_LMS_ERRORHANDELER                                                                  			    *
-  --                  |                                                                                        			    *
+  --                  |                                                                                        		    *
+  -- PACKAGE HEADER   | PKG_LMS_ERRORHANDELER                                                                  		    *
+  --                  |                                                                                        		    *
   -- PURPOSE          | PKG_LMS_ERRORHANDELER IS DESIGNED TO HANDLE AND LOG ERRORS IN A LIBRARY MANAGEMENT SYSTEM (LMS).    *
-  --                  |                                                                                        			    *
+  --                  |                                                                                        		    *
   --                  | DEFINES TWO ERROR LOGGING PROCEDURES: PR_LOG_AUTHOR_ERROR AND PR_LOG_BOOK_ERROR.              	    *
-  --                  |                                                                                        			    *
+  --                  |                                                                                        		    *
   -- ***********************************************************************************************************************+
   -- PPROCEDURE NAMES           | PR_LOG_AUTHOR_ERROR AND PR_LOG_BOOK_ERROR.                                                *
-  -- AUTHOR                     | SR CONSULTANTS                                                               			    *
-  --                            |                                                                              			    *
+  -- AUTHOR                     | SR CONSULTANTS                                                               		    *
+  --                            |                                                                              		    *
   -- MODIFICATION LOG ------------------------------------------------------------------------------------------------------+
   -- VER NO     |    DATE       |      AUTHOR       |         MODIFICATION                                                  *
   --------------+---------------+-------------------+-----------------------------------------------------------------------+
-  --																													    * 
+  --															    * 
   -- ************************************************************************************************************************
 
     -- PROCEDURE TO LOG ERRORS FOR AUTHORS
